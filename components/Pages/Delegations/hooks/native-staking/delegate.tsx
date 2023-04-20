3f81e425e357aa8b561983dd5bdcd22b9cc002b9
0xFable <0xfable@protonmail.com>
2023-04-20 12:19:38 +0100

    feat(staking): Add native delegate hook

---
+++ components/Pages/Delegations/hooks/native-staking/delegate.tsx
@@ -0,0 +1,19 @@
+import { TerraStationWallet } from 'util/wallet-adapters/terraStationWallet';
+import {Coin, Fee, MsgDelegate} from '@terra-money/feather.js';
+
+export const delegate = async (
+    wallet: TerraStationWallet,
+    destBlockchain: string,
+    valAddress: string,
+    address: string,
+    amount: number,
+    allianceDenom: string
+) => {
+    const handleMsg = new MsgDelegate(
+        address,
+        valAddress,
+        new Coin(allianceDenom, amount)
+    );
+
+    return await wallet.client.post({ chainID: destBlockchain, msgs: [handleMsg]});
+};
